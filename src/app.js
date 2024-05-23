0e8f03efde7308700b4c1df56591e0f6890ddd1b
Charles Williams <cjwillia@mit.edu>
2024-05-23 01:56:11 -0400

    added '/' recognition

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.use(express.json());
 const partRoutes = require('./routes/parts');
 app.use('/api/parts', partRoutes);
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Hello, this is the backend for the PartSelect chat agent.');
+  });
+
 // Connect to Database
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to database'))
